676de08e58591e004863fb53ccf428eb4415006b
TerrificTable <omegagaming189@gmail.com>
2022-08-19 01:03:38 +0200

    the entire text is handled in js

--- js/copyright.js
+++ js/copyright.js
@@ -1,4 +1,3 @@
 const copyright = document.getElementById("copyright");
 
-copyright.innerHTML = "&copy; " + new Date().getFullYear() + "  -  " + copyright.innerHTML;
-
+copyright.innerHTML = "&copy; " + new Date().getFullYear() + "  -  " + `NoWay Development  -  All Rights Reserved.<br>Designed by <a href="https://github.com/TerrificTable">TerrificTable</a>`;
